3220f2eac09f8a560fad67737ce4b099ece44f8f
Nick Wells <wells.nicholas@gmail.com>
2019-01-08 19:01:39 +0000

    add package-level doc and improve README

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Each of the check functions returns an error if the value doesn't pass the
 check or nil if the check is passed.
 
 The checks are typically very simple to the point where you might question
-why not perform the check directly. The reason is that these are usually
-returned as closures which allows you to pass the check to another feature.
+why not perform the check directly. The reason is that as functions they can
+be composed and combined and then passed to other code to be called later.
+They are used extensively for checking command line parameters.
---
+++ check/doc.go
@@ -0,0 +1,18 @@
+/*
+
+Package check provides a collection of atomic checks that can be applied to
+values.
+
+Each of the check functions returns an error if the value doesn't pass the
+check or nil if the check is passed. For instance a function of type check.X
+will typically take a variable of type X and return an error if the value
+does not pass the check. Where a check is parameterised there is typically a
+function which returns a check function as a closure.
+
+The checks are typically very simple to the point where you might question
+why not perform the check directly. The reason is that as functions they can
+be composed and combined and then passed to other code to be called later.
+They are used extensively for checking command line parameters.
+
+*/
+package check
