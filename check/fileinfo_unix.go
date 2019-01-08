ef3dd2135fd5b21ede54b6eed9b650f6905a1da3
Nick Wells <wells.nicholas@gmail.com>
2019-01-08 11:40:43 +0000

    Add the FileInfoOwnedBySelf func

--- check/fileinfo_unix.go
+++ check/fileinfo_unix.go
@@ -8,7 +8,20 @@ import (
 	"syscall"
 )
 
-// FileInfoUid tests that the file is owned by the specified user
+// FileInfoOwnedBySelf tests that the file is owned by the calling user
+func FileInfoOwnedBySelf(fi os.FileInfo) error {
+	stat := fi.Sys().(*syscall.Stat_t) // will panic if Sys() doesn't
+	// return a Stat_t
+	if stat.Uid == uint32(os.Getuid()) {
+		return nil
+	}
+	return fmt.Errorf("'%s' - should have been owned by the user -"+
+		" the user ID should have been %d but was %d",
+		fi.Name(), os.Getuid(), stat.Uid)
+}
+
+// FileInfoUidEQ returns a FileInfo (func) that tests that the file is owned
+// by the specified user
 func FileInfoUidEQ(uid uint32) FileInfo {
 	return func(fi os.FileInfo) error {
 		stat := fi.Sys().(*syscall.Stat_t) // will panic if Sys() doesn't
@@ -16,13 +29,13 @@ func FileInfoUidEQ(uid uint32) FileInfo {
 		if stat.Uid == uid {
 			return nil
 		}
-		return fmt.Errorf(
-			"'%s' - the user ID should have been %d but was %d",
+		return fmt.Errorf("'%s' - the user ID should have been %d but was %d",
 			fi.Name(), uid, stat.Uid)
 	}
 }
 
-// FileInfoGid tests that the file is owned by the specified user
+// FileInfoGidEQ returns a FileInfo (func) that tests that the file is owned
+// by the specified user
 func FileInfoGidEQ(gid uint32) FileInfo {
 	return func(fi os.FileInfo) error {
 		stat := fi.Sys().(*syscall.Stat_t) // will panic if Sys() doesn't
@@ -30,8 +43,7 @@ func FileInfoGidEQ(gid uint32) FileInfo {
 		if stat.Gid == gid {
 			return nil
 		}
-		return fmt.Errorf(
-			"'%s' - the group ID should have been %d but was %d",
+		return fmt.Errorf("'%s' - the group ID should have been %d but was %d",
 			fi.Name(), gid, stat.Gid)
 	}
 }
