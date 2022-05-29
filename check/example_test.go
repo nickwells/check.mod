070046e426094f9db8ae4261159facdfe1875c9f
Nick Wells <wells.nicholas@gmail.com>
2022-05-29 18:42:45 +0100

    doc: correct the example code.

--- check/example_test.go
+++ check/example_test.go
@@ -10,14 +10,14 @@ import (
 // collections of checks on a slice of strings, the first collection should
 // all pass (return a nil error) and the second set should all fail. Note
 // that the check functions called below each returns a function of type
-// check.StringSlice. For instance check.StringSliceLenEQ(2) returns a
+// check.StringSlice. For instance check.SliceLength(check.ValEQ(2)) returns a
 // check.StringSlice function that will check that the given slice is of
 // length 2. This technique is used throughout the package.
 func Example() {
 	s := []string{"hello", "world"}
 
 	passingChecks := []check.ValCk[[]string]{
-		check.SliceLength[[]string, string](check.ValEQ(2)),
+		check.SliceLength[[]string](check.ValEQ(2)),
 		check.SliceAny[[]string](
 			check.ValEQ("hello"),
 			"the list of strings must contain 'hello'"),
@@ -32,7 +32,7 @@ func Example() {
 	fmt.Println("All checks expected to pass, passed")
 
 	failingChecks := []check.ValCk[[]string]{
-		check.SliceLength[[]string, string](check.ValEQ(3)),
+		check.SliceLength[[]string](check.ValEQ(3)),
 		check.Not(
 			check.SliceHasNoDups[[]string],
 			"the list of strings must contain duplicates"),
